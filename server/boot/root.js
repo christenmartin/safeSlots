47482a8f03e4b3a0e2854956bb9ad70cacf1e470
christenmartin <christenmartin@gmail.com>
2020-04-26 19:34:44 -0700

    added store hasMany bookings relation, removed old comment

--- server/boot/root.js
+++ server/boot/root.js
@@ -10,24 +10,6 @@ module.exports = function(server) {
 
   const router = server.loopback.Router();
   router.get('/', server.loopback.status());
-  router.post('/api/bookings', (req, res) => {
-      //frontend needs to send slotId, bookingInfo 
-      //check api/slots/:id/bookings
-      // const newBooking = req.body; 
-      res.json('sigh')
-  
-      // Slots.find({ where: { "id": req.body.slotId } }, { include: 'bookings' })
-      //   .then(stuff => {
-      //       console.log(stuff);
-      //     res.json(stuff)
-      //   })
-      //   .catch(err => console.log(err));
-      // //check slot exists and check slot has not been filled since the time user clicked submit
-      // //by checking bookings.length < maxPeoplePerSlot 
-      //     //post it 
-      // //else res.json error 
-    })
-
   server.use(router);
   
 };
